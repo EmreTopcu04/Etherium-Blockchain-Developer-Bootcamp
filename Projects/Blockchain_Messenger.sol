eef095a0d69dc6a9ae078b8589eb113aa56a3525
Emre Topcu <114030199+EmreTopcu04@users.noreply.github.com>
2023-11-23 14:45:32 +0300

    Update Blockchain_Messenger.sol

--- Projects/Blockchain_Messenger.sol
+++ Projects/Blockchain_Messenger.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8;
 
+/*
+In this project, you are going to implement a simple messenger functionality in a Smart Contract.
+Through the constructor make sure you write the address that deployed the contract to a variable
+Create a message-variable of the type string, that only the deployer-address can update
+Create a counter, to see how many times the variable was updated.
+*/
+
 contract Blockchain_Messenger{
     
     int public counter;
